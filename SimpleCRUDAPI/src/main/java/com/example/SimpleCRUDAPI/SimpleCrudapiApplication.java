780dc994b1dc9f76449933a847c2604d23265825
Bereketab Haileeyesus Shanka <bereketab1994@gmail.com>
2024-12-30 06:52:32 +0100

    first commit

---
+++ SimpleCRUDAPI/src/main/java/com/example/SimpleCRUDAPI/SimpleCrudapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleCRUDAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudapiApplication.class, args);
+	}
+
+}
