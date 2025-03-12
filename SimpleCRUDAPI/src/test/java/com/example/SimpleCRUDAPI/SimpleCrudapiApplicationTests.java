4ae594d3df15e56da3171cbe8a5434883f5fc339
Bereketab Haileeyesus Shanka <151908471+bereketab24@users.noreply.github.com>
2025-03-12 19:42:31 +0100

    Update SimpleCrudapiApplicationTests.java
    Cleaning the test content.

--- SimpleCRUDAPI/src/test/java/com/example/SimpleCRUDAPI/SimpleCrudapiApplicationTests.java
+++ SimpleCRUDAPI/src/test/java/com/example/SimpleCRUDAPI/SimpleCrudapiApplicationTests.java
@@ -1,13 +1,1 @@
-package com.example.SimpleCRUDAPI;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SimpleCrudapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
